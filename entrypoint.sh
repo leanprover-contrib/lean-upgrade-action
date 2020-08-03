27de2d0f5e26327315d8099377c87611005cb044
Rob Lewis <rob.y.lewis@gmail.com>
2020-08-03 12:12:32 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 python -m pip install --upgrade pip mathlibtools
 curl https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh -sSf | sh -s -- -y
 PATH="$PATH:$HOME/.elan/bin"
-python update_or_report_error.py
+python update_or_report_error.py "${{ github.repository }}" "$1"
 
 git config user.email "leanprover.community@gmail.com"
 git config user.name "leanprover-community-bot"
 git add leanpkg.toml
 git commit -m "auto update dependencies"
-git push
+git push "$2"
