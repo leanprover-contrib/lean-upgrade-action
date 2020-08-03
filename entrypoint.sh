320b364b4d77b4085e5d7352877c341df0ef5c36
Rob Lewis <rob.y.lewis@gmail.com>
2020-08-03 12:31:18 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ python -m pip install --upgrade pip mathlibtools
 curl https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh -sSf | sh -s -- -y
 PATH="$PATH:$HOME/.elan/bin"
 python --version
+ls
 python update_or_report_error.py "$1" "$2" || exit 1
 
 git config user.email "leanprover.community@gmail.com"
